639fb5d0dab86c3c17f722675f20e43e2e434c42
stonehye <stonehye@gmail.com>
2019-01-24 09:15:38 +0000

    Save the result as a csv file

--- detector.py
+++ detector.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 import os
+import csv
 import scenedetect
 from scenedetect.video_manager import VideoManager
 from scenedetect.scene_manager import SceneManager
@@ -7,33 +8,48 @@ from scenedetect.frame_timecode import FrameTimecode
 from scenedetect.stats_manager import StatsManager
 from scenedetect.detectors import ContentDetector
 
+_CSV_FOLDER = './CSV'
 
 def extract_shots_with_pyscenedetect(src_video, threshold=0, min_scene_length=15):
-	# INFO_FILE_PATH = src_video + ".csv"
-
 	video_manager = VideoManager([src_video])
 	stats_manager = StatsManager()
 	scene_manager = SceneManager(stats_manager)
 	scene_manager.add_detector(ContentDetector(threshold, min_scene_length))
 	base_timecode = video_manager.get_base_timecode()
+	scene_list = []
 
 	try:
 		start_time = base_timecode
-		# end_time = base_timecode + 20.0
+		end_time = base_timecode + 20.0
 		video_manager.set_duration(start_time=start_time)
 		video_manager.set_downscale_factor(downscale_factor=1)
 		video_manager.start()
-		scene_manager.detect_scenes(frame_source=video_manager,frame_skip = 0)
+		scene_manager.detect_scenes(frame_source=video_manager, end_time = end_time, frame_skip = 0)
 		scene_list = scene_manager.get_scene_list(base_timecode)
-		print('List of scenes obtained:')
-		for i, scene in enumerate(scene_list):
-			print('    Scene %2d: Start %s / Frame %d, End %s / Frame %d' % (
-				i + 1,
-				scene[0].get_timecode(), scene[0].get_frames(),
-				scene[1].get_timecode(), scene[1].get_frames(),))
-
-		return scene_list
+		# print('List of scenes obtained:')
+		# for i, scene in enumerate(scene_list):
+		# 	print('    Scene %2d: Start %s / Frame %d, End %s / Frame %d' % (
+		# 		i + 1,
+		# 		scene[0].get_timecode(), scene[0].get_frames(),
+		# 		scene[1].get_timecode(), scene[1].get_frames(),))
+		write_csv(src_video, scene_list)
 	except:
 		print("error")
 	finally:
 		video_manager.release()
+	return scene_list
+
+
+def write_csv(src_video, scene_list):
+	video_name = src_video.split('/')[2]
+	video_name = video_name.split('.')[0] + '.csv'
+	csv_path = os.path.join(_CSV_FOLDER, video_name)
+	print(csv_path)
+	f = open(csv_path, 'w', encoding='utf-8', newline='')
+	wr = csv.writer(f)
+	wr.writerow(["Start Frame", "End Frame", "Start Time", "End Time"])
+	for i, scene in enumerate(scene_list):
+		start_frame_num = "%08d" %(scene[0].get_frames())
+		end_frame_num = "%08d" % (scene[1].get_frames())
+		wr.writerow([start_frame_num, end_frame_num, scene[0].get_timecode(), scene[1].get_timecode()])
+	f.close()
--- main.py
+++ main.py
@@ -6,5 +6,7 @@ _FOLDER = "video"
 pyscenedetect_threshold = 30
 
 video_set = "./{}/*.mp4".format(_FOLDER)
+cnt = 1
 for video_path in glob.glob(video_set):
+	print("Current Video: {}".format(video_path))
 	psd_predictions = extract_shots_with_pyscenedetect(video_path, threshold=pyscenedetect_threshold, min_scene_length=15)
